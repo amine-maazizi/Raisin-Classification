5e1cd479b904351ebae0530f77920ca9501983f7
amine-maazizi <Aminema1000@gmail.com>
2025-05-23 19:04:57 +0200

    Revised Unsupervised learning (EDA, PCA)

--- src/partie_1.R
+++ src/partie_1.R
@@ -18,6 +18,24 @@ p = ncol(data)
 
 colSums(is.na(data))
 
+# Vérification des valeurs revendiquées par l'article
+morph_vars <- c("Area", "Perimeter", "MajorAxisLength", "MinorAxisLength", 
+                "Eccentricity", "ConvexArea", "Extent")
+
+# Calcul des statistiques descriptives avec sapply
+stats <- data.frame(
+  Variable = morph_vars,
+  Minimum = sapply(morph_vars, function(var) min(data[[var]], na.rm = TRUE)),
+  Maximum = sapply(morph_vars, function(var) max(data[[var]], na.rm = TRUE)),
+  Moyenne = sapply(morph_vars, function(var) mean(data[[var]], na.rm = TRUE))
+)
+
+# Affichage des résultats avec formatage
+cat("Statistiques descriptives des variables morphologiques :\n")
+stats[, c("Minimum", "Maximum", "Moyenne")] <- round(stats[, c("Minimum", "Maximum", "Moyenne")], 3)
+print(stats)  
+cat("\n")
+
 library(ggplot2)
 library(reshape2)
 library(corrplot)
@@ -25,36 +43,17 @@ library(car)
 library(GGally)
 
 # Analyse univariée et bivariée
-# Boxplot toutes variables
-png("plots/partie_1/analyse_uni_et_bi_variee/boxplot_all.png")
-boxplot(data[,-p])
-dev.off()
-
-# Boxplot Area
-p1 <- ggplot(data) + geom_boxplot(aes(y=Area))
-ggsave("plots/partie_1/analyse_uni_et_bi_variee/boxplot_area.png", plot = p1)
-
-# Boxplot Area par Class
-p2 <- ggplot(data) +
-  geom_boxplot(aes(x=as.factor(Class), y=Area, col=as.factor(Class))) +
-  xlab("Class") + ylab("Area")
-ggsave("plots/partie_1/analyse_uni_et_bi_variee/boxplot_area_class.png", plot = p2)
-
-# Boxplots multiples
+# Boxplots facettés
 data_mod <- melt(data, id.vars="Class", measure.vars=1:(p-1))
-p3 <- ggplot(data_mod) +
-  geom_boxplot(aes(x=variable, y=value, color=variable)) +
-  xlab("Variables") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-ggsave("plots/partie_1/analyse_uni_et_bi_variee/boxplot_multi.png", plot = p3)
-
-# Variance
-round(apply(data[,-p], 2, var), 2)
-
-# Scatterplot matrice
-png("plots/partie_1/analyse_uni_et_bi_variee/pairs_plot.png")
-pairs(data[,-p])
-dev.off()
+p0 <- ggplot(data_mod, aes(x = as.factor(Class), y = value, fill = as.factor(Class))) +
+  geom_boxplot() +
+  facet_wrap(~variable, scales = "free_y") +  # Échelle libre pour l'axe y
+  xlab("Classe") + ylab("Valeur") +
+  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
+  labs(fill = "Classe") +  # Légende pour les couleurs
+  theme(legend.position = "top")  # Position de la légende
+p0
+ggsave("plots/partie_1/analyse_uni_et_bi_variee/boxplot_all_faceted_by_class.png", plot = p0)
 
 # Corrélation
 round(cor(data[,-p]), 2)
@@ -64,18 +63,9 @@ png("plots/partie_1/analyse_uni_et_bi_variee/corrplot.png")
 corrplot(cor(data[,-p]), method="circle")
 dev.off()
 
-# Scatterplot matrice (car)
-png("plots/partie_1/analyse_uni_et_bi_variee/scatterplot_matrix_car.png")
-scatterplotMatrix(as.matrix(data[,-p]))
-dev.off()
-
-# GGpairs quantitatives
-p4 <- ggpairs(data[,-p])
-ggsave("plots/partie_1/analyse_uni_et_bi_variee/ggpairs_quant.png", plot = p4)
-
 # GGpairs avec Class
-p5 <- ggpairs(data, ggplot2::aes(colour=as.factor(Class)))
-ggsave("plots/partie_1/analyse_uni_et_bi_variee/ggpairs_class.png", plot = p5)
+p1 <- ggpairs(data, ggplot2::aes(colour=as.factor(Class)))
+ggsave("plots/partie_1/analyse_uni_et_bi_variee/ggpairs_class.png", plot = p1)
 
 
 # ACP
@@ -83,7 +73,8 @@ library(FactoMineR)
 library(cowplot)
 library(factoextra)
 
-res <- PCA(data[,-p], scale.unit = TRUE, graph = FALSE)
+class_vector <- data$Class      
+res <- PCA(data[, -p], scale.unit = TRUE, graph = FALSE)
 
 # a - Valeurs et vecteurs propres
 round(res$eig, 4)
@@ -93,115 +84,79 @@ p1 <- fviz_eig(res, addlabels = TRUE, main = "Éboulis des valeurs propres") +
   geom_hline(yintercept = 100/p)
 ggsave("plots/partie_1/ACP/scree_plot.jpg", plot = p1)
 
-# b - Cercle des corrélations
-p2 <- plot(res, choix = "var", axes = c(1,2), graph.type = "classic")
+
+# Create ggplot-based correlation circle plots
+p2 <- fviz_pca_var(res, axes = c(1, 2), repel = TRUE)
+p3 <- fviz_pca_var(res, axes = c(2, 3), repel = TRUE)
+
+# Save them using ggsave
 ggsave("plots/partie_1/ACP/cor_circle_1_2.jpg", plot = p2)
-p3 <- plot(res, choix = "var", axes = c(2,3), graph.type = "classic")
 ggsave("plots/partie_1/ACP/cor_circle_2_3.jpg", plot = p3)
 
-# c - Analyse simultanée individus/variables
-p4 <- plot(res, choix = "ind", axes = c(1,2), label = "none", graph.type = "classic")
-p5 <- plot(res, choix = "var", axes = c(1,2), graph.type = "classic")
-ggsave("plots/partie_1/ACP/ind_var_plan1_2.jpg", plot = plot_grid(p4, p5, ncol = 2))
+# Plan 1-2 : individus colorés par classe et variables
+p_ind_1_2 <- fviz_pca_ind(res, 
+                          habillage = as.factor(class_vector), 
+                          addEllipses = TRUE, 
+                          label = "none", 
+                          repel = TRUE)
 
-Ind <- res$ind
-indi <- as.numeric(names(head(sort(Ind$contrib[,1], decreasing = TRUE), 7)))
-p6 <- plot(res, choix = "ind", axes = c(1,2), label = "none", graph.type = "classic") +
-  geom_point(aes(x = Ind$coord[indi,1], y = Ind$coord[indi,2]), col = "red", size = 2) +
-  geom_text(aes(x = Ind$coord[indi,1], y = Ind$coord[indi,2]), label = indi, col = 2, nudge_y = 0.1)
-ggsave("plots/partie_1/ACP/ind_contrib_plan1_2.jpg", plot = p6)
 
-# Contributions par axe
-apply(Ind$contrib, 2, which.max)
-head(sort(Ind$contrib[,1], decreasing = TRUE), 7)
+ggsave("plots/partie_1/ACP/ind_plan1_2.jpg", plot = p_ind_1_2, width = 12, height = 6)
 
-# Meilleure représentation axe 2
-which.max(Ind$cos2[,2])
 
-# Plan (2,3)
-p7 <- plot(res, choix = "ind", axes = c(2,3), label = "none", graph.type = "classic") +
-  geom_point(aes(x = Ind$coord[indi,2], y = Ind$coord[indi,3]), col = "red", size = 2) +
-  geom_text(aes(x = Ind$coord[indi,2], y = Ind$coord[indi,3]), label = indi, col = 2, nudge_y = 0.1)
-p8 <- plot(res, choix = "var", axes = c(2,3), graph.type = "classic")
-ggsave("plots/partie_1/ACP/ind_var_plan2_3.jpg", plot = plot_grid(p7, p8))
+# Plan 2-3 : même chose avec axes 2 et 3
+p_ind_2_3 <- fviz_pca_ind(res, 
+                          habillage = as.factor(class_vector), 
+                          axes = c(2, 3), 
+                          addEllipses = TRUE, 
+                          label = "none", 
+                          repel = TRUE)
 
-# Contributif et mal représenté
-indj <- which(res$ind$cos2[,2] + res$ind$cos2[,3] < 0.15 & 
-                (res$ind$coord[,2]^2 + res$ind$coord[,3]^2) > mean(res$ind$coord[,2]^2 + res$ind$coord[,3]^2))
-p9 <- plot(res, choix = "ind", axes = c(2,3), label = "none", graph.type = "classic") +
-  geom_point(aes(x = Ind$coord[indj,2], y = Ind$coord[indj,3]), col = "purple", size = 2) +
-  geom_text(aes(x = Ind$coord[indj,2], y = Ind$coord[indj,3]), label = indj, col = "purple", nudge_y = 0.1)
-ggsave("plots/partie_1/ACP/ind_contrib_mal_plan2_3.jpg", plot = p9)
 
-# Peu contributif et bien représenté
-which(res$ind$cos2[,2] > 0.85 & res$ind$contrib[,2] < 0.25)
+ggsave("plots/partie_1/ACP/ind_plan_2_3.jpg", plot = p_ind_2_3, width = 12, height = 6)
 
-# Contributif et bien représenté
-which(res$ind$cos2[,2] > 0.95 & res$ind$contrib[,2] > 0.85)
-
-# Individus supplémentaires
-df <- as.data.frame(lapply(data, function(x) tapply(x, data$Class, mean)))
-indsup <- nrow(data) + 1:nrow(df)
-data[indsup,] <- df
-res_sup <- PCA(data[,-p], ind.sup = indsup, graph = FALSE)
-p10 <- plot(res_sup, choix = "ind", axes = c(1,2), label = "ind.sup", graph.type = "classic")
-p11 <- plot(res_sup, choix = "ind", axes = c(2,3), label = "ind.sup", graph.type = "classic")
-ggsave("plots/partie_1/ACP/ind_sup_plan1_2.jpg", plot = p10)
-ggsave("plots/partie_1/ACP/ind_sup_plan2_3.jpg", plot = p11)
-
-# Variable qualitative Class
-data$Class <- as.factor(data$Class)
-res_qual <- PCA(data, quali.sup = p, graph = FALSE)
-
-# Graphique avec ggplot2
-p12_data <- data.frame(
-  x = res_qual$ind$coord[,1],
-  y = res_qual$ind$coord[,2],
-  Class = data$Class[1:nrow(res_qual$ind$coord)]
-)
-p12_supp <- data.frame(
-  x = res_qual$quali.sup$coord[,1],
-  y = res_qual$quali.sup$coord[,2]
-)
-p12 <- ggplot() +
-  geom_point(data = p12_data, aes(x = x, y = y, color = Class)) +
-  geom_point(data = p12_supp, aes(x = x, y = y), color = "blue", size = 3) +
-  labs(x = "Dim 1", y = "Dim 2", title = "Plan (1,2) avec Class")
-ggsave("plots/partie_1/ACP/ind_quali_plan1_2.jpg", plot = p12)
-
-p13_data <- data.frame(
-  x = res_qual$ind$coord[,2],
-  y = res_qual$ind$coord[,3],
-  Class = data$Class[1:nrow(res_qual$ind$coord)]
-)
-p13_supp <- data.frame(
-  x = res_qual$quali.sup$coord[,2],
-  y = res_qual$quali.sup$coord[,3]
-)
-p13 <- ggplot() +
-  geom_point(data = p13_data, aes(x = x, y = y, color = Class)) +
-  geom_point(data = p13_supp, aes(x = x, y = y), color = "blue", size = 3) +
-  labs(x = "Dim 2", y = "Dim 3", title = "Plan (2,3) avec Class")
-ggsave("plots/partie_1/ACP/ind_quali_plan2_3.jpg", plot = p13)
 
 
 # CAH
 library(cluster)
 library(mclust)
+library(ggplot2)
+library(ggdendro)
 
+# Données
 data_quant <- data[,-p]
 variety <- as.factor(data$Class)
 
-res_cah <- hclust(dist(data_quant), method = "complete")
+# CAH avec méthode de Ward
+res_cah <- hclust(dist(data_quant), method = "ward.D2")
 
-jpeg("plots/partie_1/CAH/dendrogram_complete.jpg")
-plot(res_cah, cex = 0.5, main = "Dendrogramme CAH (complete)")
-abline(h = 15000)
-dev.off()
+# Récupération des sauts d'inertie (delta_t = hauteur des fusions)
+delta_t <- diff(res_cah$height)
 
-grp_h15000 <- cutree(res_cah, h = 15000)
-nb_groups <- length(unique(grp_h15000))
-cat("Nombre de groupes à h=15000 :", nb_groups, "\n")
+# Tracé du diagramme en bâtons inversé des sauts d'inertie
+df_delta <- data.frame(etape = 1:length(delta_t), delta = rev(delta_t))
+
+gg_delta <- ggplot(df_delta, aes(x = etape, y = delta)) +
+  geom_bar(stat = "identity", fill = "steelblue") +
+  labs(title = "Diagramme des sauts d'inertie (méthode du coude)",
+       x = "Étapes de fusion (du dernier au premier)", y = expression(delta[t])) +
+  theme_minimal()
+
+ggsave("plots/partie_1/CAH/diagramme_sauts_inertie.jpg", gg_delta,
+       width = 8, height = 5, dpi = 300)
+
+print(gg_delta)
+
+# Optionnel : Affichage du dendrogramme (Ward)
+dendro_data <- dendro_data(res_cah)
+gg_dendro <- ggplot() +
+  geom_segment(data = dendro_data$segments, 
+               aes(x = x, y = y, xend = xend, yend = yend)) +
+  theme_minimal() +
+  labs(title = "Dendrogramme CAH (méthode Ward)", x = "", y = "Distance")
+ggsave("plots/partie_1/CAH/dendrogramme_ward.jpg", gg_dendro,
+       width = 10, height = 6, dpi = 300)
+print(gg_dendro)
 
 sil <- silhouette(cutree(res_cah, k = 2), dist(data_quant))
 p_sil <- fviz_silhouette(sil, print.summary = FALSE)
